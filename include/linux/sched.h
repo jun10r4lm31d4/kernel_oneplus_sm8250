d69bfe094c6cac0136ea73a5256065c39e191d88
Juansheng <jaycopter@foxmail.com>
2024-05-19 16:27:34 +0800

    cpuset: Unify cpus_allowed and cpus_requested
    `cpus_requested` was added by Google several years ago and from that
    commit, it was for vendor usage to mask cpu usage. But the function had
    been substituted by `cpus_allowed` in so many subsequent patches.
    
    Unify them to make life easier.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -817,7 +817,6 @@ struct task_struct {
 	unsigned long			max_allowed_capacity;
 	int				nr_cpus_allowed;
 	cpumask_t			cpus_allowed;
-	cpumask_t			cpus_requested;
 
 #ifdef CONFIG_PREEMPT_RCU
 	int				rcu_read_lock_nesting;
